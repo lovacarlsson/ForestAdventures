3f1a56e9b8c1884c5c91207db614c7f6b48853a9
Rufus <rufustrukhin@gmail.com>
2022-09-22 19:37:57 +0200

    PostSave

--- Assets/PlantScript.cs
+++ Assets/PlantScript.cs
@@ -6,10 +6,13 @@ public class PlantScript : MonoBehaviour
 {
     public int damage = 1;
     private bool isAlive = true;
+    private bool isClose = false;
     private Animator animator;
     PlayerMovement Player;
     [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip deathclip;
+
+    public Transform player, plant;
 
     private void Start(){
         animator = gameObject.GetComponent<Animator>();
@@ -17,6 +20,29 @@ public class PlantScript : MonoBehaviour
     }
     private void Update(){
         animator.SetBool("isAlive", isAlive);
+        animator.SetBool("isClose", isClose);
+
+        StartAttack();
+    }
+
+    private void StartAttack(){
+        if(Vector2.Distance(player.position, plant.position)<5f){
+            isClose = true;
+            gameObject.GetComponent<CapsuleCollider2D>().enabled = true;
+            if(this.animator.GetCurrentAnimatorStateInfo(0).IsName("PlantAttack")){
+               StartCoroutine(Attack(100f)); 
+            }
+        }
+        else{
+            isClose = false;
+            gameObject.GetComponent<CapsuleCollider2D>().enabled = false;
+        }
+    }
+
+    IEnumerator Attack(float timeOut){
+        gameObject.GetComponent<CapsuleCollider2D>().enabled = false;
+        isClose = false;
+        yield return new WaitForSeconds(timeOut);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -34,7 +60,7 @@ public class PlantScript : MonoBehaviour
         {
             
             isAlive = false;
-            gameObject.GetComponent<PolygonCollider2D>().enabled = false;
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
             Player.JumpCall();
             audioSource.PlayOneShot(deathclip); 
             Invoke("DestroyEnemy", 0.25f);
