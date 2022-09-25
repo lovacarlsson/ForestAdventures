6416c15031e83e2a483d3434e469880453166e92
lovacarlsson <112973903+lovacarlsson@users.noreply.github.com>
2022-09-25 21:03:22 +0200

    Slutfix
    Level change 2-3
    Checkpoint ljud

--- Assets/Scripts/Quest_DoorToNextLevel.cs
+++ Assets/Scripts/Quest_DoorToNextLevel.cs
@@ -10,19 +10,18 @@ public class Quest_DoorToNextLevel : MonoBehaviour
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip levelClearedClip;
 
-
-
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") == true)
         {
-            if (Quest_Player.isQuestComplete == true) {
+            //if (Quest_Player.isQuestComplete == true) {
                 GameObserver.SaveStarsToMemory(collision.GetComponent <PlayerState>().starAmount);
                 audioSource.PlayOneShot(levelClearedClip);
-                SceneManager.LoadScene("Level02");
+                SceneManager.LoadScene(levelToLoad);
 
-            }
+           // }
         }
     }
 }
