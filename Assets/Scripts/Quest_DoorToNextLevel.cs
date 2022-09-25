fcfc545bc564d6d1e85c6332085fa514d796ffc9
19artr <19artr@stockholmscience.se>
2022-09-26 01:01:25 +0200

    Revert "Update Quest_DoorToNextLevel.cs"
    This reverts commit 3b3beeeb0d2ea0d950dfc465d0283d66559c7c88.

--- Assets/Scripts/Quest_DoorToNextLevel.cs
+++ Assets/Scripts/Quest_DoorToNextLevel.cs
@@ -10,23 +10,18 @@ public class Quest_DoorToNextLevel : MonoBehaviour
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip levelClearedClip;
 
-    Quest_Giver quest_Giver;
-    PlayerState playerState;
+    
 
-    void Start(){
-        quest_Giver = GameObject.Find("QuestGiverChest").GetComponent<Quest_Giver>();
-        playerState = GameObject.Find("Player").GetComponent<PlayerState>();
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(quest_Giver.amountToCollect <= playerState.starAmount){
-            GameObserver.SaveStarsToMemory(collision.GetComponent<PlayerState>().starAmount);
-            audioSource.PlayOneShot(levelClearedClip);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-        else if(SceneManager.GetActiveScene().buildIndex == 2){
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
+        if (collision.CompareTag("Player") == true)
+        {
+            //if (Quest_Player.isQuestComplete == true) {
+                GameObserver.SaveStarsToMemory(collision.GetComponent <PlayerState>().starAmount);
+                audioSource.PlayOneShot(levelClearedClip);
+                SceneManager.LoadScene(levelToLoad);
 
+           // }
+        }
     }
 }
